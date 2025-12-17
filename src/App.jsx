8e903d66b99af193e7e663592a7cf25844054176
ola <hollabodey.kilaso@gmail.com>
2025-12-17 20:08:35 +0000

    Add Welcome component and update dependencies
    - Introduced a new Welcome component to enhance the application interface.
    - Updated package.json and package-lock.json to include new dependencies: gsap and lucide-react.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import Navbar from "./components /Navbar"
+import { Navbar } from "#components"
 
 const App = () => {
   return (
     <main>
       <Navbar />
+      <Welcome />
     </main>
   )
 }
